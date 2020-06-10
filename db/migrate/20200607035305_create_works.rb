b56112fd219c8e15adbdaf0dd4bfd92f9d8b2b6e
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-09 21:53:04 -0700

    updadted migrations

---
+++ db/migrate/20200607035305_create_works.rb
@@ -0,0 +1,12 @@
+class CreateWorks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :works do |t|
+      t.string :category
+      t.string :title
+      t.string :creator
+      t.date :publication_year
+      t.string :description
+      t.timestamps
+    end
+  end
+end
