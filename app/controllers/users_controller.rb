dea749a857cd33f7f3f53986f4172454d26c4cca
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-10 00:51:06 -0700

    added index, create, and vote

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,69 @@
 class UsersController < ApplicationController
 
   def index
-
+    @users = User.all
   end
 
-  def new
-
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      flash[:error] = "User does not exist"
+      redirect_to new_user_path
+    end
   end
 
-  def create
-
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user 
+      session[:user_id] = user.id
+      flash[:success] = "You've successfully logged in as #{username}"
+      return
+    else
+      user = User.create(username: username)
+      if user.save
+        session[:user_id] = user.id
+        flash[:success] = "You've successfully created a new user; #{username}"
+      else
+        flash[:error] = "Error, username can't be left blank"
+        redirect_to login_path
+        return
+      end
+    end
+    redirect_to root_path
   end
 
-  def update
-
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "#{user.username}, you have successfully loffed out"
+      else
+        session[:user_id] = nil
+        flash[:error] = "An error occured while tying to logout"
+      end
+    else
+      flash[:error] = "You must be logged in order to logout"
+    end
+    redirect_to root_path
   end
 
-  def destroy
-    
+  def current
+    @current_user = User.find_by(id: session[:user_id])
   end
 
+  private
+  def find_user
+    user_id = params[:id].to_i
+    @user = User.find_by(id: user_id)
+  end
 
+  def check_user
+    if @user.nil?
+      redirect_to users_path, error: 'User not found'
+      return
+    end
+  end
 
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,23 +1,27 @@
 class VotesController < ApplicationController
 
   def index
-
-  end
-
-  def new
-
+    @votes = Vote.all
   end
 
   def create
-
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @vote = Vote.new(user_id: @user.id, work_id: params[:work_id])
+      if @vote.save
+        flash[:success] = "Successfully upvoted for #{@work.title}."
+      else
+        flash[:error] = "Looks like you've already voted for #{@work.title}."
+        redirect_to works_path
+        return
+      end
+    elsif session[:user_id] == nil
+      flash[:error] = "Error! You must logged in to vote."
+      redirect_to works_path
   end
 
-  def update
-
-  end
-
-  def destroy
-    
+  def vote_params
+    return params.require(:vote).permit(:user_id, :work_id)
   end
 
 end
