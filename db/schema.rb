7d180edfdf83305cff8d8b0c70649f1db5a25ff0
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-10 14:23:03 -0700

    no changes made

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_035428) do
+ActiveRecord::Schema.define(version: 2020_06_07_035428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "votes", force: :cascade do |t|
-    t.bigint "work_id"
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "works", force: :cascade do |t|
-    t.string "category"
-    t.string "title"
-    t.string "creator"
-    t.integer "publication_year"
-    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
