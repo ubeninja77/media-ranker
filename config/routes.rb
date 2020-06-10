6ef522adca1cf3ef873938d0a918133fad3b2771
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-09 22:32:15 -0700

    added and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+
+  resources :users
+
+  resources :works do
+    resources :votes, only: [:create]
+  end
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  
+  get "/users/current", to: "users#current", as: "current_user"
+  
+  post "/works/:id/vote", to: "votes#create", as:"upvote"
+  
+  get '/homepages', to: 'homepages#index', as: 'homepages'
+  root to: "homepage#index"
+
 end
